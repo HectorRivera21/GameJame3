007944aea8cab620446d3fd7a732546c8ba5ec3f
Hector <redmagix2000@gmail.com>
2024-09-28 19:24:46 -0500

    hallo

---
+++ Stealth/Assets/Scripts/PatrolScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolScript : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int targetPoint;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == waypoints[targetPoint].position)
+        {
+            increaseTargetInt();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[targetPoint].position, speed);
+    }
+
+    void increaseTargetInt()
+    {
+        targetPoint++;
+        if (targetPoint >= waypoints.Length)
+        {
+            Debug.Log("here");
+            targetPoint = 0;
+        }
+    }
+}
